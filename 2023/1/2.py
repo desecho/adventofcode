aa2b6beabc40aa6bfb7f20656cea0eebf9b5852b
Anton Samarchyan <desecho@gmail.com>
2023-12-02 09:36:23 -0500

    Changes

--- 2023/1/2.py
+++ 2023/1/2.py
@@ -4,6 +4,17 @@ def load_lines(filename):
     with open(filename) as file:
         return file.read().splitlines()
 
+EXTRA_DIGITS = {
+    "twone": 21,
+    "sevenine": 79,
+    "eightwo": 82,
+    "eighthree": 83,
+    "threeight": 38,
+    "oneight": 18,
+    "fiveight": 58,
+    "nineight": 98,
+}
+
 DIGITS = {
     "one": 1,
     "two": 2,
@@ -16,21 +27,21 @@ DIGITS = {
     "nine": 9,
 }
 
-def convert_digits(value: str):
+def convert_digits(digits, value: str):
     poss = []
-    for key, digit in DIGITS.items():
+    for key, digit in digits.items():
         pos = value.find(key)
         if pos != -1:
             poss.append((pos, key, digit))
     poss.sort(key=lambda x: x[0])
     final_value = value
-    for pos, key, digit in poss:
-        final_value = final_value.replace(key, str(digit), 1)
+    if len(poss) > 0:
+        final_value = final_value.replace(poss[0][1], str(poss[0][2]), 1)
 
     if final_value == value:
         return final_value
 
-    return convert_digits(final_value)
+    return convert_digits(digits, final_value)
 
 def find_first_digit(value):
     for char in value:
@@ -50,13 +61,10 @@ if __name__ == '__main__':
     lines = load_lines(FILENAME)
     result = 0
     for line in lines:
-        print(line)
-        line = convert_digits(line)
-        print(line)
+        line = convert_digits(EXTRA_DIGITS, line)
+        line = convert_digits(DIGITS, line)
         x = find_first_digit(line)
         y = find_second_digit(line)
-        print(x + y)
-        print()
         result += int(x + y)
 
     print(result)
