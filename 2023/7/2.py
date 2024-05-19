c47c40b8ea61b8b0727aa200d21aa6e35c6564ed
Anton Samarchyan <desecho@gmail.com>
2024-05-19 10:02:24 -0400

    Changes

--- 2023/7/2.py
+++ 2023/7/2.py
@@ -6,29 +6,27 @@ def load_lines(filename):
     with open(filename) as file:
         return file.read().splitlines()
 
-def get_unique_cards(cards_set):
-    unique_cards = []
-    for card in cards_set:
-        if card not in unique_cards:
-            unique_cards.append(card)
-    return unique_cards
+def get_unique_set(set):
+    unique_set = []
+    for card in set:
+        if card not in unique_set:
+            unique_set.append(card)
+    return unique_set
 
-def process_set(cards_set):
-    if cards_set == "JJJJJ":
+def process_set(set: str):
+    sets = [set]
+    if set == "JJJJJ":
         return ["JJJJJ"]
-    output_sets = []
-    unique_cards = get_unique_cards(cards_set)
-    if "J" not in unique_cards:
-        return [cards_set]
-    unique_cards.remove("J")
-    for card in unique_cards:
-        cards_set_copy = cards_set.replace("J", card)
-        output_sets.append(cards_set_copy)
-    return output_sets
+    unique_set = get_unique_set(set)
+    for card in unique_set:
+        new_set = set.replace("J", card)
+        sets.append(new_set)
+    return sets
 
 def is_five_of_a_kind(sets):
     for set in sets:
-        if set[0] == set[1] == set[2] == set[3] == set[4]:
+        result =  set[0] == set[1] == set[2] == set[3] == set[4]
+        if result == True:
             return True
     return False
 
@@ -79,22 +77,21 @@ def get_score(set):
 
 def get_sort_value(set):
     processed_set = process_set(set[0])
-    unprocessed_set = [set[0]]
     if is_five_of_a_kind(processed_set):
-        return 900000000000000000000000000000000000000000000 + get_score(set[0])
+        return 10000000000000000000000 + get_score(set[0])
     elif is_four_of_a_kind(processed_set):
-        return 80000000000000000000 + get_score(set[0])
+        return 90000000000000000000 + get_score(set[0])
     elif is_full_house(processed_set):
-        return 600000000000000000 + get_score(set[0])
+        return 700000000000000000 + get_score(set[0])
     elif is_three_of_a_kind(processed_set):
-        return 6000000000000 + get_score(set[0])
+        return 6000000000000000 + get_score(set[0])
     elif is_two_pairs(processed_set):
-        return 4000000000000 + get_score(set[0])
+        return 50000000000000 + get_score(set[0])
     elif is_one_pair(processed_set):
-        return 2000000000000 + get_score(set[0])
-
+        return 400000000000 + get_score(set[0])
     else:
-        return get_score(set[0])
+        return 1000000000 + get_score(set[0])
+
 if __name__ == '__main__':
     lines = load_lines(FILENAME)
     sets = []
@@ -104,6 +101,5 @@ if __name__ == '__main__':
     sets.sort(key=get_sort_value)
     result = 0
     for x in range(1, len(sets) + 1):
-        # print(x, sets[x-1])
         result += x * int(sets[x-1][1])
     print(result)
