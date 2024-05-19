8876a033f89d3cea7ea5a967755bd3375ea6cbed
Anton Samarchyan <desecho@gmail.com>
2024-05-18 20:36:47 -0400

    changes

--- 2023/7/2.py
+++ 2023/7/2.py
@@ -60,8 +60,7 @@ def is_two_pairs(sets):
         for card in set:
             if set.count(card) == 2:
                 set_copy = set.replace(card, "")
-                if set_copy[0] == set_copy[1]:
-                    return True
+                return is_one_pair([set_copy])
     return False
 
 def is_one_pair(sets):
@@ -76,40 +75,26 @@ def get_score(set):
     set = set[::-1]
     for x in range(len(set)):
         score += CARDS.index(set[x]) * (100 ** (x+1))
-    if set == "JJJJJ":
-        print(set, score)
     return score
 
 def get_sort_value(set):
     processed_set = process_set(set[0])
     unprocessed_set = [set[0]]
-    if is_five_of_a_kind(unprocessed_set):
-        return 100000000000000000000000000000000000000000000000 + get_score(set[0])
     if is_five_of_a_kind(processed_set):
         return 900000000000000000000000000000000000000000000 + get_score(set[0])
-    elif is_four_of_a_kind(unprocessed_set):
-        return 90000000000000000000 + get_score(set[0])
     elif is_four_of_a_kind(processed_set):
         return 80000000000000000000 + get_score(set[0])
-    elif is_full_house(unprocessed_set):
-        return 700000000000000000 + get_score(set[0])
     elif is_full_house(processed_set):
         return 600000000000000000 + get_score(set[0])
-    elif is_three_of_a_kind(unprocessed_set):
-        return 6000000000000000 + get_score(set[0])
     elif is_three_of_a_kind(processed_set):
-        return 5000000000000000 + get_score(set[0])
-    elif is_two_pairs(unprocessed_set):
-        return 50000000000000 + get_score(set[0])
+        return 6000000000000 + get_score(set[0])
     elif is_two_pairs(processed_set):
-        return 40000000000000 + get_score(set[0])
-    elif is_one_pair(unprocessed_set):
-        return 400000000000 + get_score(set[0])
+        return 4000000000000 + get_score(set[0])
     elif is_one_pair(processed_set):
-        return 300000000000 + get_score(set[0])
-    else:
-        return 1000000000 + get_score(set[0])
+        return 2000000000000 + get_score(set[0])
 
+    else:
+        return get_score(set[0])
 if __name__ == '__main__':
     lines = load_lines(FILENAME)
     sets = []
@@ -119,6 +104,6 @@ if __name__ == '__main__':
     sets.sort(key=get_sort_value)
     result = 0
     for x in range(1, len(sets) + 1):
-        print(x, sets[x-1])
+        # print(x, sets[x-1])
         result += x * int(sets[x-1][1])
     print(result)
