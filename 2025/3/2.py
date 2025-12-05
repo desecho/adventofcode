92fb9868267696f4b9f190516f0b95bfc84dda41
Anton Samarchyan <desecho@gmail.com>
2025-12-04 21:17:38 -0500

    Changes

--- 2025/3/2.py
+++ 2025/3/2.py
@@ -1,42 +1,26 @@
 FILENAME = "input.txt"
-
+DIGITS = 12
 
 def load_data():
     with open(FILENAME) as file:
         return file.read().splitlines()
 
 
-
-def has_number(n, x):
-    n = str(n)
-    n1 = n[0]
-    n2 = n[1]
-    try:
-        i = x.index(n1)
-    except ValueError:
-        return False
-    indexes = [i for i, z in enumerate(x) if z == n2]
-    for j in indexes:
-        if j > i:
-            return True
-
-    return False
-
-
-def get_max(n):
-    for x in range(99, 9, -1):
-        if "0" in str(x):
-            continue
-        if has_number(x, n):
-            return x
-    raise Exception("FAILED")
-
+def get_max(n, answer):
+    if len(answer) == 12:
+        return int(answer)
+    for x in range(9, 0, -1):
+        indexes = [i for i, z in enumerate(n) if int(z) == x]
+        for i in indexes:
+            if i + DIGITS <= len(n) + len(answer):
+                answer += str(x)
+                return get_max(n[i+1:], answer)
 
 if __name__ == "__main__":
     data = load_data()
     sum = 0
     for x in data:
-        z = get_max(x)
+        z = get_max(x, "")
         sum += z
 
     print(sum)
